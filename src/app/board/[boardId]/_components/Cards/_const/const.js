2c6c694721baeb257dcb30c4260fe4e07859bbff
Austin <awkjacobs@gmail.com>
2025-01-08 17:10:41 -0500

    Add documentation and improve type safety for CARD_SIZE constant

--- src/app/board/[boardId]/_components/Cards/_const/const.js
+++ src/app/board/[boardId]/_components/Cards/_const/const.js
@@ -1,3 +1,11 @@
+/**
+ * Defines card dimensions for different sizes
+ * @type {Object.<string, string>}
+ * @property {string} null - Default size
+ * @property {string} 1 - Small size
+ * @property {string} 2 - Medium size
+ * @property {string} 3 - Large size
+ */
 export const CARD_SIZE = {
     null: "h-20 md:h-24",
     1: "h-20 md:h-24",
