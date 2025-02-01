63d05737e45a81a80bebac8c13db453c800bd30b
Austin <awkjacobs@gmail.com>
2025-02-01 17:21:01 -0500

    Correct Middleware route matcher for the board dynamic route

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,20 +1,27 @@
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server"
 import { NextResponse } from "next/server"
 
-const isProtectedRoute = createRouteMatcher(["/home", "/board/[boardId])"])
+const isProtectedRoute = createRouteMatcher(["/home", "/board/(.*)"])
 const isAdminRoute = createRouteMatcher(["/admin(.*)"])
 
-export default clerkMiddleware(async (auth, req) => {
-    if (isProtectedRoute(req)) await auth.protect()
+export default clerkMiddleware(
+    async (auth, req) => {
+        const { userId, redirectToSignIn } = await auth()
+        console.log(isProtectedRoute(req))
+        if (!userId && isProtectedRoute(req)) {
+            return redirectToSignIn()
+        }
 
-    if (
-        isAdminRoute(req) &&
-        (await auth()).sessionClaims?.metadata?.role !== "admin"
-    ) {
-        const url = new URL("/", req.url)
-        return NextResponse.redirect(url)
-    }
-})
+        if (
+            isAdminRoute(req) &&
+            (await auth()).sessionClaims?.metadata?.role !== "admin"
+        ) {
+            const url = new URL("/", req.url)
+            return NextResponse.redirect(url)
+        }
+    },
+    { debug: true },
+)
 
 export const config = {
     matcher: [
