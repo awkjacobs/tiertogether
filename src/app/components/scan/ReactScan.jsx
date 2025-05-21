e1ba0ad53ea8cdb3e7de95489f0130884f7ef5f4
Austin <awkjacobs@gmail.com>
2025-05-21 16:31:11 -0400

    update scan configuration to enable only in development mode

--- src/app/components/scan/ReactScan.jsx
+++ src/app/components/scan/ReactScan.jsx
@@ -6,7 +6,7 @@ import { JSX, useEffect } from "react"
 export function ReactScan() {
     useEffect(() => {
         scan({
-            enabled: true,
+            enabled: process.env.NODE_ENV === "development",
         })
     }, [])
 
